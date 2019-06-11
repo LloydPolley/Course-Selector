4cf52406582d28858ad0cbdb7d0b83ae64ad3c22
lloydpolley <lloydpolley@googlemail.com>
2019-06-11 11:57:28 +0100

    herouk install

--- server/server.js
+++ server/server.js
@@ -4,3 +4,11 @@ const path = require('path');
 const publicPath = path.join(__dirname, '..', 'build');
 
 app.use(express.static(publicPath));
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(publicPath, 'index.html'))
+});
+
+app.listen(4000, () => {
+    console.log('running server');
+})
