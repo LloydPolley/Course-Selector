4a08ca27edecc1ab41c1f1d1150681eca945ef3a
lloydpolley <lloydpolley@googlemail.com>
2019-06-14 13:18:58 +0100

    after firebase unmerge

--- src/store/store.js
+++ src/store/store.js
@@ -23,20 +23,20 @@ store.subscribe(() => {
   console.log("Redux Store", state);
 });
 
-store.dispatch(addCourse({courseLang: "fr", courseName: "tutorial 1", coursePrice: 1000, courseLength: "2 week", courseDescription:"dfghjk", school: "Taunton"}));
-store.dispatch(addCourse({courseLang: "en", courseName: "tutorial 2", coursePrice: 500, courseLength: "4 week", courseDescription:"aaa", school: "Oxford"}));
-store.dispatch(addCourse({courseLang: "de", courseName: "tutorial 3", coursePrice: 465, courseLength: "5 week", courseDescription:"sss", school: "London"}));
-store.dispatch(addCourse({courseLang: "fr", courseName: "tutorial 4", coursePrice: 386, courseLength: "6 week", courseDescription:"ddd", school: "Cambridge"}));
-store.dispatch(addCourse({courseLang: "fr", courseName: "tutorial 15", coursePrice: 673, courseLength: "8 week", courseDescription:"ccc", school: "Oxford"}));
+// store.dispatch(addCourse({courseLang: "fr", courseName: "tutorial 1", coursePrice: 1000, courseLength: "2 week", courseDescription:"dfghjk", school: "Taunton"}));
+// store.dispatch(addCourse({courseLang: "en", courseName: "tutorial 2", coursePrice: 500, courseLength: "4 week", courseDescription:"aaa", school: "Oxford"}));
+// store.dispatch(addCourse({courseLang: "de", courseName: "tutorial 3", coursePrice: 465, courseLength: "5 week", courseDescription:"sss", school: "London"}));
+// store.dispatch(addCourse({courseLang: "fr", courseName: "tutorial 4", coursePrice: 386, courseLength: "6 week", courseDescription:"ddd", school: "Cambridge"}));
+// store.dispatch(addCourse({courseLang: "fr", courseName: "tutorial 15", coursePrice: 673, courseLength: "8 week", courseDescription:"ccc", school: "Oxford"}));
  
 
-store.dispatch(addSchool('London'));
-store.dispatch(addSchool('Oxford'));
-store.dispatch(addSchool('Taunton'));
-store.dispatch(addSchool('Cambridge'));
-store.dispatch(addSchool('Boston'));
+// store.dispatch(addSchool('London'));
+// store.dispatch(addSchool('Oxford'));
+// store.dispatch(addSchool('Taunton'));
+// store.dispatch(addSchool('Cambridge'));
+// store.dispatch(addSchool('Boston'));
 
-store.dispatch(sortBySchool());
+// store.dispatch(sortBySchool());
 
 // store.dispatch(getSchools());
 
